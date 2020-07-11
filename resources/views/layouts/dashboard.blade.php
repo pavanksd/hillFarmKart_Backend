da749ae131303e2008640d8803e37386f252404a
Pavan <pavan@mail.petoo.in>
2020-07-11 21:33:53 +0530

    fix in logout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -98,6 +98,9 @@
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     {{ __('Logout') }}
                                 </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                </form>
                             </div>
                         </li>
                     </ul>
